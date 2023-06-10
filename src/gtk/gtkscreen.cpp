23fa5bdacaa6a07ce35c1b54f15b62d1c32af434
revvv <8734113+revvv@users.noreply.github.com>
2023-06-10 16:33:22 +0200

    Fix issue #1: editor crash w/o particle effects

--- src/gtk/gtkscreen.cpp
+++ src/gtk/gtkscreen.cpp
@@ -75,8 +75,15 @@ void GTKScreen::configure_size() {
          * automatically by cairo. */
         if (gd_particle_effects)
             back.reset(cairo_image_surface_create(CAIRO_FORMAT_RGB24, w, h));
-        else
-            back.reset(gdk_window_create_similar_surface(gtk_widget_get_window(drawing_area), CAIRO_CONTENT_COLOR, w, h));
+        else {
+            if (gtk_widget_get_window(drawing_area) != NULL) {
+                back.reset(gdk_window_create_similar_surface(gtk_widget_get_window(drawing_area), CAIRO_CONTENT_COLOR, w, h));
+            } else {
+                // FIX Issue #1
+                gd_message("configure_size: window w/o particle effects");
+                back.reset(cairo_image_surface_create(CAIRO_FORMAT_RGB24, w, h));
+            }
+        }
     } else {
         /* rendering to a software buffer. */
         back.reset(cairo_image_surface_create(CAIRO_FORMAT_RGB24, w, h));
