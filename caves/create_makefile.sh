b7c9f525b6cba5d250e45b3f466e8010cbc761c2
revvv <8734113+revvv@users.noreply.github.com>
2023-08-25 14:52:34 +0200

    update cave list: subdirs now allowed

--- caves/create_makefile.sh
+++ caves/create_makefile.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-SUBDIRS=$(find . -type d -printf "%f\n"|grep -v "\.$")
+SUBDIRS=$(find . -type d | sed 's!^./!!' | grep -v "\.$")
+
 echo "# Automatically generated Makefile.am! Check $0" >Makefile.am
 echo >>Makefile.am
 
 for a in $SUBDIRS; do
     CAVES=$(find $a -type f)
-    echo ${a}dir = '$(pkgdatadir)'/caves/${a} >> Makefile.am
-    echo ${a}_CAVES = $CAVES >>Makefile.am
-    DIST=$DIST\ \$\(${a}_CAVES\)
-    echo ${a}_DATA = \$\(${a}_CAVES\) >>Makefile.am
+    echo ${a/\//_}dir = '$(pkgdatadir)'/caves/${a/\//_} >> Makefile.am
+    echo ${a/\//_}_CAVES = $CAVES >>Makefile.am
+    DIST=$DIST\ \$\(${a/\//_}_CAVES\)
+    echo ${a/\//_}_DATA = \$\(${a/\//_}_CAVES\) >>Makefile.am
     echo >>Makefile.am
 done
 echo EXTRA_DIST = $(basename $0) $DIST >>Makefile.am
