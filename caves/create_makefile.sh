c764eab9f3a076cbb1994bf2a930be56f84a8838
revvv <8734113+revvv@users.noreply.github.com>
2023-08-27 11:43:07 +0200

    fix caves with subdirs #46

--- caves/create_makefile.sh
+++ caves/create_makefile.sh
@@ -5,7 +5,7 @@ echo "# Automatically generated Makefile.am! Check $0" >Makefile.am
 echo >>Makefile.am
 
 for a in $SUBDIRS; do
-    CAVES=$(find $a -type f)
+    CAVES=$(find $a -maxdepth 1 -type f)
     echo ${a/\//_}dir = '$(pkgdatadir)'/caves/${a/\//_} >> Makefile.am
     echo ${a/\//_}_CAVES = $CAVES >>Makefile.am
     DIST=$DIST\ \$\(${a/\//_}_CAVES\)
