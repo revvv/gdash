ef9c5ae76b8daf0a4bc9db619d2c82e5442df195
revvv <8734113+revvv@users.noreply.github.com>
2023-09-09 10:17:57 +0200

    remove unused setting BorderProperties.fix

--- src/cave/caverenderedengine.cpp
+++ src/cave/caverenderedengine.cpp
@@ -692,42 +692,39 @@ inline void CaveRendered::store(int x, int y, GdDirectionEnum dir, GdElementEnum
 inline void CaveRendered::move(int x, int y, GdDirectionEnum dir, GdElementEnum element) {
 
     // fix: screen wrapped boulder should not kill player instantly
-    //if (border_fix) // fix seems OK, so always active
-    {
-        //gd_message("XXX x=%d y=%d e=%d current=%d dir=%d gravity=%d xd=%d yd=%d", x, y, element, get(x, y), dir, gravity, x + gd_dx[dir], y + gd_dy[dir]);
-        if (y + gd_dy[dir] == map.height() && dir == MV_DOWN) {
-            // height is out of bounds, but because of screen wrap it's the first line again
-            switch (get(x, y)) {
-            case O_STONE:
-            case O_MEGA_STONE:
-            case O_DIAMOND:
-            case O_NUT:
-            case O_FLYING_STONE:
-            case O_FLYING_DIAMOND:
-            case O_FALLING_WALL:
-            case O_NITRO_PACK:
-                store(x, y, element); // falling element
-                return; // do not move element
-            default:
-                break;
-            }
+    //gd_message("XXX x=%d y=%d e=%d current=%d dir=%d gravity=%d xd=%d yd=%d", x, y, element, get(x, y), dir, gravity, x + gd_dx[dir], y + gd_dy[dir]);
+    if (y + gd_dy[dir] == map.height() && dir == MV_DOWN) {
+        // height is out of bounds, but because of screen wrap it's the first line again
+        switch (get(x, y)) {
+        case O_STONE:
+        case O_MEGA_STONE:
+        case O_DIAMOND:
+        case O_NUT:
+        case O_FLYING_STONE:
+        case O_FLYING_DIAMOND:
+        case O_FALLING_WALL:
+        case O_NITRO_PACK:
+            store(x, y, element); // falling element
+            return; // do not move element
+        default:
+            break;
         }
-        else if (x + gd_dx[dir] == map.width() && dir == MV_RIGHT) {
-            // width is out of bounds, but because of screen wrap it's the first column again
-            switch (get(x, y)) {
-            case O_STONE:
-            case O_MEGA_STONE:
-            case O_DIAMOND:
-            case O_NUT:
-            case O_FLYING_STONE:
-            case O_FLYING_DIAMOND:
-            case O_FALLING_WALL:
-            case O_NITRO_PACK:
-                store(x, y, element); // falling element
-                return; // do not move element
-            default:
-                break;
-            }
+    }
+    else if (x + gd_dx[dir] == map.width() && dir == MV_RIGHT) {
+        // width is out of bounds, but because of screen wrap it's the first column again
+        switch (get(x, y)) {
+        case O_STONE:
+        case O_MEGA_STONE:
+        case O_DIAMOND:
+        case O_NUT:
+        case O_FLYING_STONE:
+        case O_FLYING_DIAMOND:
+        case O_FALLING_WALL:
+        case O_NITRO_PACK:
+            store(x, y, element); // falling element
+            return; // do not move element
+        default:
+            break;
         }
     }
 
